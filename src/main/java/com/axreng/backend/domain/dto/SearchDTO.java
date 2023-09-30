3a8fa7fd0952b4b88d3ceaa9bbab79c81c5474ef
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-09-30 15:12:34 -0300

    fix: refactor dto

--- src/main/java/com/axreng/backend/domain/dto/SearchDTO.java
+++ src/main/java/com/axreng/backend/domain/dto/SearchDTO.java
@@ -8,23 +8,17 @@ import com.axreng.backend.domain.model.URLAddress;
 
 public class SearchDTO {
     private final String id;
-    private final String keyword;
     private final String status;
     private final List<String> urls;
 
     public SearchDTO(Search search) {
         this.id = search.getId().getValue();
-        this.keyword = search.getKeyword().getValue();
         this.status = search.getStatus().getValue();
         this.urls = search.getUrls().stream().map(URLAddress::getValue).collect(Collectors.toList());
     }
 
     public String getId() {
         return id;
-    }
-
-    public String getKeyword() {
-        return keyword;
     }
 
     public String getStatus() {
