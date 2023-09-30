d2adf7bfb0389dcfdb588dda9ab351cd0a29a086
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-09-30 14:23:57 -0300

    feat: adds default url

--- src/main/java/com/axreng/backend/infra/AppConfig.java
+++ src/main/java/com/axreng/backend/infra/AppConfig.java
@@ -4,8 +4,9 @@ public class AppConfig {
     private final String baseUrl;
 
     public AppConfig() {
-        this.baseUrl = System.getenv("BASE_URL"); 
-       
+        this.baseUrl = System.getenv("BASE_URL") != null 
+        ? System.getenv("BASE_URL") 
+        : "https://github.com/arthurgabriel73"; 
     }
 
     public String getBaseUrl() {
