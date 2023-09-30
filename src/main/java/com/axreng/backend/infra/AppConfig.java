e7ca4c3273fc43a5aff3602fcd75882db8baaab3
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-09-30 14:40:24 -0300

    fix: fixes env origin default url

--- src/main/java/com/axreng/backend/infra/AppConfig.java
+++ src/main/java/com/axreng/backend/infra/AppConfig.java
@@ -1,36 +1,12 @@
 package com.axreng.backend.infra;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import static com.axreng.backend.util.AnsiColors.*;
-
 public class AppConfig {
-    private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
-    private static final String DEFAULT_BASE_URL = "https://github.com/arthurgabriel73";
-    private String baseUrl;
+    private final String baseUrl;
 
     public AppConfig() {
-        this.baseUrl = loadPropertiesOrDefault();
-    }
-
-    private String loadPropertiesOrDefault() {
-        try (InputStream input = getClass().getClassLoader().getResourceAsStream("application.properties")) {
-            Properties prop = new Properties();
-            if (input == null) {
-                logger.error("{}Sorry, unable to find application.properties, using default base url", RED.getCode());
-                return DEFAULT_BASE_URL;
-            }
-            prop.load(input);
-            return prop.getProperty("default.BASE_URL", DEFAULT_BASE_URL);
-        } catch (IOException ex) {
-            logger.error("{}Error loading application properties: {}", RED.getCode(), ex.getMessage());
-            return DEFAULT_BASE_URL;
-        }
+        this.baseUrl = System.getenv("BASE_URL") != null 
+            ? System.getenv("BASE_URL") 
+            : "https://github.com/arthurgabriel73";
     }
 
     public String getBaseUrl() {
