4e732d33fdc874b6f35f8d57c57c28467f1d87cd
Arthur Gabriel <arthurgabriel73@gmail.com>
2023-09-30 11:21:49 -0300

    fix: fixes useCase to execute various search at time

--- src/main/java/com/axreng/backend/application/usecase/CreateSearchUseCase.java
+++ src/main/java/com/axreng/backend/application/usecase/CreateSearchUseCase.java
@@ -5,22 +5,28 @@ import com.axreng.backend.application.webcrawler.CrawlingService;
 import com.axreng.backend.domain.model.Keyword;
 import com.axreng.backend.domain.model.Search;
 import com.axreng.backend.domain.model.URLAddress;
+import com.axreng.backend.infra.AppConfig;
+import com.axreng.backend.infra.DefaultWebCrawler;
 
 import java.util.concurrent.CompletableFuture;
 
 public class CreateSearchUseCase {
     private final SearchRepository repository;
-    private final CrawlingService crawlingService;
+    private CrawlingService crawlingService;
 
-    public CreateSearchUseCase(SearchRepository repository, CrawlingService crawlingService) {
+    public CreateSearchUseCase(SearchRepository repository) {
         this.repository = repository;
-        this.crawlingService = crawlingService;
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        crawlingService = new DefaultWebCrawler(new AppConfig().getBaseUrl(), numThreads);
     }
 
     public Search execute(String keyword, int limit) {
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        crawlingService = new DefaultWebCrawler(new AppConfig().getBaseUrl(), numThreads);
         Search search = new Search(new Keyword(keyword));
         CompletableFuture<Void> crawlCompletion = crawlingService.crawl(keyword, limit, url -> {
             search.addURL(new URLAddress(url));
+            System.out.println(url);
             repository.upsert(search);
         });
         crawlCompletion.thenApplyAsync(result -> {
--- src/main/java/com/axreng/backend/infra/factory/SearchControllerFactory.java
+++ src/main/java/com/axreng/backend/infra/factory/SearchControllerFactory.java
@@ -3,7 +3,6 @@ package com.axreng.backend.infra.factory;
 import com.axreng.backend.application.repository.SearchRepository;
 import com.axreng.backend.application.usecase.CreateSearchUseCase;
 import com.axreng.backend.application.usecase.GetSearchUseCase;
-import com.axreng.backend.application.webcrawler.CrawlingService;
 import com.axreng.backend.infra.AppConfig;
 import com.axreng.backend.infra.DefaultWebCrawler;
 import com.axreng.backend.infra.http.CreateSearchController;
@@ -14,10 +13,9 @@ public class SearchControllerFactory {
 
     private SearchControllerFactory() {}
 
-    static int numThreads = Runtime.getRuntime().availableProcessors();
-    static CrawlingService crawlingService = new DefaultWebCrawler(new AppConfig().getBaseUrl(), numThreads);
+    
     static SearchRepository repository = new InMemorySearchRepository();
-    static CreateSearchUseCase createSearchUseCase = new CreateSearchUseCase(repository, crawlingService);
+    static CreateSearchUseCase createSearchUseCase = new CreateSearchUseCase(repository);
     static GetSearchUseCase getSearchUseCase = new GetSearchUseCase(repository);
 
     public static GetSearchController getSearchController() {
